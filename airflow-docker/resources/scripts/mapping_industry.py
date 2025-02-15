3a24d9a679b80baf66fbb6bdfb84565064bd8c84
Fitran Dwi Pramakrsina <fitranpramakrisna@gmail.com>
2025-02-15 11:28:40 +0700

    create mapping for industry name normalization

---
+++ airflow-docker/resources/scripts/mapping_industry.py
@@ -0,0 +1,76 @@
+industry_mapping = {
+    "Advertising And Marketing": "Advertising and Media",
+
+    "Automotive and Manufacturing": "Automotive",
+    "Motor Vehicle Manufacturing": "Automotive",
+    "Otomotive": "Automotive",
+
+    "Banking & Finance": "Banking",
+    
+    "E-commerce and Online Retail": "E-Commerce",
+    "ECommerce": "E-Commerce",
+    
+    "Education Administration Programs": "Education",
+    "Education, Technology, Human Resource, Consulting": "Education",
+    
+    "Finance Services": "Financial Technology",
+    "Finance Technology": "Financial Technology",
+    "Financial Service": "Financial Technology",
+    "Financial Services": "Financial Technology",
+    "Finance Technology,":"Financial Technology",
+    "Tech Company, Fintech": "Financial Technology",
+    
+    "Consumer Goods": "FMCG",
+    "Fast Moving Consumer Good": "FMCG",
+    "Multinational Consulting / FMCG": "FMCG",
+    "Research, Data, Consulting, Finance, FMCG (Fast Moving Consumer Goods)": "FMCG",
+    
+    "Food & Beverage": "Food And Beverage Services",
+    "Food And Beverage Manufacturing": "Food And Beverage Services",
+    "Food And Beverages": "Food And Beverage Services",
+    
+    "Healthcare Technology": "Healthcare and Medical Services",
+    "Hospitals And Health Care": "Healthcare and Medical Services",
+    
+    "Consulting and Human Resources": "Human Resources Services",
+    "Human Resources and Consulting": "Human Resources Services",
+    
+    "Computer & Information Technology": "Information Technology & Services",
+    "Information Technology": "Information Technology & Services",
+    "Information Technology / IT": "Information Technology & Services",
+    "Information Tecnology": "Information Technology & Services",
+    "Technology (Software Development)": "Information Technology & Services",
+    "Technology Education": "Information Technology & Services",
+    "Technology Services": "Information Technology & Services",
+    "Technology and Software Solutions": "Information Technology & Services",
+    "Technology, Information And Internet": "Information Technology & Services",
+    "Technology, Information, Internet": "Information Technology & Services",
+    "IT And Software": "Information Technology & Services",
+    "Software Development": "Information Technology & Services",
+    "Teknologi": "Information Technology & Services",
+    "technology services": "Information Technology & Services",
+    
+    "IT Consultant": "IT Consulting and Services",
+    "IT Consulting & Services Company": "IT Consulting and Services",
+    "IT Services & IT Consulting": "IT Consulting and Services",
+    "IT Services And IT Consulting": "IT Consulting and Services",
+    "Konsultan": "IT Consulting and Services",
+    
+    "Appliances, Electrical, And Electronics Manufacturing": "Manufacture",
+    "Manufacturing": "Manufacture",
+    "Footwear Manufacturing": "Manufacture",
+    
+    "Media Production": "Media & Entertainment",
+    
+    "Manufacturing (Pharmaceutical)": "Pharmaceutical",
+    "Pharmaceutical Manufacturing": "Pharmaceutical",
+    
+    "Logistics": "Transport & Logistics",
+    "Transportation, Logistics, Supply Chain And Storage": "Transport & Logistics",
+    "Truck Rental Services": "Transport & Logistics",
+    "Truck Transportation": "Transport & Logistics",
+    
+    "Retail Apparel And Fashion": "Retail",
+    "Retail Luxury Goods And Jewelry": "Retail",
+    "Retail and fashion": "Retail"
+}
