3bb5729dcf549b82066d76255ce4dedcc46946ee
Fitran Dwi Pramakrsina <fitranpramakrisna@gmail.com>
2025-02-19 00:06:05 +0700

    bug fix mapping work level type

--- airflow-docker/resources/scripts/transform_kalibrr.py
+++ airflow-docker/resources/scripts/transform_kalibrr.py
@@ -1,13 +1,8 @@
 def tranform_web_kalibrr(df):
     import pandas as pd
-    
-    print(df)
-    df = pd.read_json(df)
-    print(df)
-    # df['city'] = df['city'].astype(str).apply(lambda x: 'Jakarta' if 'Jakarta' in x else x)
 
-    # standarisasi nama kota
-    # df['city'] = df['city'].apply(lambda x: 'Jakarta' if 'Jakarta' in x else x)
+    df = pd.read_json(df)
+    
     df['city'] = df['city'].apply(lambda x: 'Jakarta' if isinstance(x, str) and 'Jakarta' in x else x)
     df['city'] = df['city'].str.replace(r'\b(Regency|Kota)\b', '', regex=True).str.strip()
     
@@ -21,23 +16,20 @@ def tranform_web_kalibrr(df):
 
     df['education_min_lvl'] = df['education_min_lvl'].map(education_mapping).fillna('Other')
     
-    # standarisasi level_experience
-    working_lvl_experience_mapping = {
-        200: 'Freshgrad / Junior',
-        400: 'Mid-Senior Level Manager',
-        300: 'Supervisor / Asisten Manager',
-        100: 'Intern'
+    def mapping_working_lvl(row):
+        working_lvl = row['level_experience']
+        
+        if working_lvl == 200:
+            return 'Fresh Grad / Junior'
+        elif working_lvl == 400:
+            return 'Mid-Senior Level Manager'
+        elif working_lvl == 400:
+            return 'Supervisor / Asisten Manager'
+        elif working_lvl == 100:
+            return 'Intern'
+        else:
+            'Other'
 
-    }
-    df['level_experience'] = df['level_experience'].apply(lambda x: x[0] if isinstance(x, list) and x else 'Other')
-    df['level_experience'] = df['level_experience'].map(working_lvl_experience_mapping).fillna('Other')
+    df['level_experience'] = df.apply(mapping_working_lvl, axis=1)
 
-
-    # df['level_experience'] = df['level_experience'].map(working_lvl_experience_mapping).fillna('Other')
-    # print(df['min_salary'].unique())
-    # print(df['max_salary'].unique())
-    
-    # return df.to_excel('transformed_kalibrr.xlsx', index=False)
-    # print(df)
-    print(f"total baris : {df.shape[0]}")
     return df.to_json()
